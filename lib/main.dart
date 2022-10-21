1eab24d652d1687a4c18f4657113afaae9a8b0a9
MohamedSaeed <106338523+mohamedsaeed873@users.noreply.github.com>
2022-10-21 12:45:42 -0700

    done

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islami/screens/splashScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Islami',
+      theme: ThemeData(
+     
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
